2a3ece186d322c239b85942720483990802cf554
Jila <jilakhaghani@gmail.com>
2021-06-14 15:52:33 +1000

    Lift up table state

--- src/App.js
+++ src/App.js
@@ -7,10 +7,63 @@ const axios = require('axios');
 class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            rows: this.createRows(),
+            columns: [
+                {
+                    key: 'id',
+                    name: 'ID'
+                },
+                {
+                    key: 'car_make',
+                    name: 'Make'
+                },
+                {
+                    key: 'car_model',
+                    name: 'Model'
+                },
+                {
+                    key: 'year',
+                    name: 'Year'
+                },
+                {
+                    key: 'variant',
+                    name: 'Variant'
+                }
+            ]
+        };
 
         this.saveForm = this.saveForm.bind(this);
         this.handleFormInputChange = this.handleFormInputChange.bind(this);
         this.handleFormTypeAheadInputChange = this.handleFormTypeAheadInputChange.bind(this);
+    }
+
+    createRows() {
+        const rows = [];
+        const url = process.env.REACT_APP_CARES_GUIDE_API+'/car';
+        let temp = [];
+
+        axios({
+            method: 'get',
+            url: url,
+        }).then(function (response)  {
+            for (let j = 0; j < response.data.rows.length; j++) {
+                temp = Object.assign({
+                    "id": response.data.rows[j].id,
+                    "year": response.data.rows[j].year,
+                    "variant":  response.data.rows[j].variant,
+                    "car_make": response.data.rows[j]. car_make.make,
+                    "car_model": response.data.rows[j]. car_model.model,
+                });
+                rows.push(temp);
+            }
+        }).catch(function (error) {
+            if (error.response && error.response.status === 400) {
+                alert(error.response.data.message);
+            }
+        });
+
+        return rows;
     }
 
     saveForm(event) {
@@ -23,9 +76,7 @@ class App extends Component {
             url: url,
             data: this.state
         }).then(function (response)  {
-            self.setState({});
             event.target.reset();
-            //ReactDOM.unmountComponentAtNode(document.getElementById("make_id"));
         }).catch(function (error) {
             if (error.response && error.response.status === 400) {
                 alert(error.response.data.message);
@@ -58,7 +109,7 @@ class App extends Component {
                     />
                 </div>
                 <div className="row">
-                    <Table />
+                    <Table rows={this.state.rows} columns={this.state.columns}/>
                 </div>
             </div>
         );
--- src/Table.js
+++ src/Table.js
@@ -7,64 +7,11 @@ class Table extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            rows: this.createRows(),
-            columns: [
-                {
-                    key: 'id',
-                    name: 'ID'
-                },
-                {
-                    key: 'car_make',
-                    name: 'Make'
-                },
-                {
-                    key: 'car_model',
-                    name: 'Model'
-                },
-                {
-                    key: 'year',
-                    name: 'Year'
-                },
-                {
-                    key: 'variant',
-                    name: 'Variant'
-                }
-            ]
-        };
-    }
-
-    createRows() {
-        const rows = [];
-        const url = process.env.REACT_APP_CARES_GUIDE_API+'/car';
-        let temp = [];
-
-        axios({
-            method: 'get',
-            url: url,
-        }).then(function (response)  {
-            for (let j = 0; j < response.data.rows.length; j++) {
-                temp = Object.assign({
-                    "id": response.data.rows[j].id,
-                    "year": response.data.rows[j].year,
-                    "variant":  response.data.rows[j].variant,
-                    "car_make": response.data.rows[j]. car_make.make,
-                    "car_model": response.data.rows[j]. car_model.model,
-                });
-                rows.push(temp);
-            }
-        }).catch(function (error) {
-            if (error.response && error.response.status === 400) {
-                alert(error.response.data.message);
-            }
-        });
-
-        return rows;
     }
 
     render() {
         return (
-            <DataGrid columns={this.state.columns} rowsCount={this.state.rows.length} rows={this.state.rows} />
+            <DataGrid columns={this.props.columns} rowsCount={this.props.rows.length} rows={this.props.rows} />
         );
     }
 }
